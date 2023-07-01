43e1aac23909bbe97bec153517e3a37f02c385b9
Wish <With@example.com>
2023-07-01 15:54:04 +0900

    Add GORM

--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 
 	"github.com/labstack/echo/v4"
+	"github.com/kelseyhightower/envconfig"
 )
+
+type Config struct {
+	MySQL MySQL
+}
+type MySQL struct {
+    Host string
+    Port string
+    User string
+	Password string
+	Database string
+}
 
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
 
 func main() {
+	var config Config
+	envconfig.Process("APP", &config)
+
+	fmt.Println(config.MySQL)
+
+	dsn := config.MySQL.User + ":@tcp(" + config.MySQL.Host + ":" + config.MySQL.Port + ")/" + config.MySQL.Database + "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
 	e := echo.New()
 
 	e.GET("/", hello)
